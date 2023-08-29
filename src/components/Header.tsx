adfda6eaf8d6eab9f9a11562f76e0fe94ceeb109
SamSKS <samuel.meireles.15s@outlook.com>
2023-08-29 20:35:51 -0300

    feat: :sparkles: update Header with Container

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,18 @@
 import styles from "./Header.module.css";
 
 import rocketIcon from "../assets/layer_rocket.svg";
+import { Container } from "./Container";
 
 export function Header() {
   return (
-    <header className={styles.header}>
-      <img src={rocketIcon} alt="rocket icon" />
-      <div>
-        <strong>to</strong>
-        <strong>do</strong>
-      </div>
+    <header>
+      <Container className={styles.header}>
+        <img src={rocketIcon} alt="rocket icon" />
+        <div>
+          <strong>to</strong>
+          <strong>do</strong>
+        </div>
+      </Container>
     </header>
   );
 }

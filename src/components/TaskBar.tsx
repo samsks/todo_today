833ab7fa0cf4fbc9b45fdb6eade84cd3b3033774
SamSKS <samuel.meireles.15s@outlook.com>
2023-08-31 13:17:55 -0300

    feat: :sparkles: insert new task

--- src/components/TaskBar.tsx
+++ src/components/TaskBar.tsx
@@ -1,11 +1,36 @@
+import { useState } from "react";
 import { Container } from "./Container";
 import styles from "./TaskBar.module.css";
 import { AiOutlinePlusCircle } from "react-icons/ai";
 
+interface Task {
+  id: number;
+  title: string;
+  isComplete: boolean;
+}
+
 export function TaskBar() {
+  const [taskList, setTaskList] = useState<Task[]>([]);
+
+  function handleCreateNewTask(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const input = e.currentTarget[0] as HTMLInputElement;
+
+    setTaskList([
+      ...taskList,
+      {
+        id: taskList[taskList.length - 1]?.id + 1 || 1,
+        title: input.value,
+        isComplete: false,
+      },
+    ]);
+
+    input.value = "";
+  }
+
   return (
     <Container>
-      <form className={styles.taskBar}>
+      <form onSubmit={handleCreateNewTask} className={styles.taskBar}>
         <input type="text" placeholder="Adicione uma nova tarefa" />
         <button type="submit">
           Criar

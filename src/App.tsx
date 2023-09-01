6db8a354185cf9cd0a03b6795ef3e69573e897ca
SamSKS <samuel.meireles.15s@outlook.com>
2023-09-01 00:32:52 -0300

    feat: :sparkles: insert task interface and state in App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,23 @@ import "./global.css";
 import styles from "./App.module.css";
 import { TaskBar } from "./components/TaskBar";
 import { TaskBox } from "./components/TaskBox";
+import { useState } from "react";
+
+export interface iTask {
+  id: number;
+  title: string;
+  isComplete: boolean;
+}
 
 function App() {
+  const [taskList, setTaskList] = useState<iTask[]>([]);
+
   return (
     <div>
       <Header />
       <main className={styles.bodyTasks}>
-        <TaskBar />
-        <TaskBox />
+        <TaskBar taskList={taskList} setTaskList={setTaskList} />
+        <TaskBox taskList={taskList} setTaskList={setTaskList} />
       </main>
     </div>
   );

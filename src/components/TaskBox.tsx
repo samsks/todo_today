da50136c56ebb3df6c33d43a3e19330cf3e6ed90
SamSKS <samuel.meireles.15s@outlook.com>
2023-09-01 00:35:43 -0300

    feat: :sparkles: insert logic for TaskBox Component

--- src/components/TaskBox.tsx
+++ src/components/TaskBox.tsx
@@ -1,12 +1,30 @@
+// import { BlankTask } from "./BlankTask";
+import { iTask } from "../App";
+import { BlankTask } from "./BlankTask";
 import { Container } from "./Container";
 import { SummaryTask } from "./SummaryTask";
 import styles from "./TaskBox.module.css";
+import { TaskCard } from "./TaskCard";
 
-export function TaskBox() {
+interface TaskBoxProps {
+  taskList: iTask[];
+  setTaskList: React.Dispatch<React.SetStateAction<iTask[]>>;
+}
+
+export function TaskBox({ taskList, setTaskList }: TaskBoxProps) {
   return (
     <Container>
       <section className={styles.taskBox}>
-        <SummaryTask />
+        <SummaryTask taskList={taskList} />
+        {taskList.length === 0 ? (
+          <BlankTask />
+        ) : (
+          <section className={styles.taskList}>
+            {taskList.map((task) => (
+              <TaskCard key={task.id} task={task} setTaskList={setTaskList} />
+            ))}
+          </section>
+        )}
       </section>
     </Container>
   );

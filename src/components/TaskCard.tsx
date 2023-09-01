4292dcab54fab3998474ba7c3129458e3267dc51
SamSKS <samuel.meireles.15s@outlook.com>
2023-09-01 00:37:49 -0300

    feat: :sparkles: insert logic for Task component

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,25 +1,47 @@
+import { iTask } from "../App";
 import styles from "./TaskCard.module.css";
 
 import { HiOutlineTrash } from "react-icons/hi";
 
-export function TaskCard() {
+interface TaskCardProps {
+  task: iTask;
+  setTaskList: React.Dispatch<React.SetStateAction<iTask[]>>;
+}
+
+export function TaskCard({ task, setTaskList }: TaskCardProps) {
+  const handleCheckBoxChange = () => {
+    setTaskList((stateTaskList) =>
+      stateTaskList.map((stateTask) =>
+        stateTask.id === task.id
+          ? { ...stateTask, isComplete: !stateTask.isComplete }
+          : stateTask
+      )
+    );
+  };
+
+  const handleDeleteTask = () => {
+    setTaskList((stateTaskList) =>
+      stateTaskList.filter((stateTask) => stateTask.id !== task.id)
+    );
+  };
+
   return (
     <div
-      className={[styles.taskCard, styles.checkText]
+      className={[styles.taskCard, task.isComplete && styles.checkText]
         .filter((className) => !!className)
         .join(" ")}
     >
       <label className={styles.checkContainer}>
-        <input type="checkbox" />
+        <input
+          type="checkbox"
+          checked={task.isComplete}
+          onChange={handleCheckBoxChange}
+        />
         <span className={styles.checkmark}></span>
       </label>
 
-      <p>
-        Teste de tarefa sdad asd hfgh af fergrece cercer vtvbec verecrv
-        ecervtrbe crece cercxew sdfsta ffnrebvc dfw safasd asdas asdasdas
-        asdasdasd asdasd asdasd fsdfd sdfasdf sadfsad fgasdfsdfsdf dfsdfsd
-      </p>
-      <div>
+      <p>{task.title}</p>
+      <div onClick={handleDeleteTask}>
         <HiOutlineTrash className={styles.trashButton} />
       </div>
     </div>
